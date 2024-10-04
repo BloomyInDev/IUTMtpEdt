a025ad0486793d834807b5d400d7dee66b114b25
Bloomy <90511192+BloomyInDev@users.noreply.github.com>
2024-10-04 15:24:34 +0200

    J'ai oublié de commit ^^'
    - Le scrapping fonctionne
    
    - TSV parser modifié pour faire par classe au lieu de par années

--- scrapper/tsv_parser.py
+++ scrapper/tsv_parser.py
@@ -1,4 +1,4 @@
-def OpenRawTSV(tsv_name: str) -> str:
+def openRawTSV(tsv_name: str) -> str:
 	file = open(tsv_name, "r", encoding="utf8")
 	raw_text = file.read()
 	file.close()
@@ -7,10 +7,10 @@ def OpenRawTSV(tsv_name: str) -> str:
 
 	return raw_text
 
-def ParseRawText(raw_text: str, header_line: bool=True) -> dict[int: str]:
+def parseRawText(raw_text: str, header_line: bool=True) -> dict[str: str]:
 	lines: list[str] = raw_text.split("\n")
 
-	output: dict[int: str] = {}
+	output: dict[str: str] = {}
 
 	if (header_line):
 		lines.remove(lines[0])
@@ -21,14 +21,14 @@ def ParseRawText(raw_text: str, header_line: bool=True) -> dict[int: str]:
 	for l in lines:
 		l: list[str] = l.split("\t")
 
-		year_id: int = int(l[0])
+		classe: str = l[0]
 		url: str = l[1]
 
-		output[year_id] = url
+		output[classe] = url
 
 	return output
 
-def ParseTSV(tsv_filename: str, header_line: bool=True) -> dict[int: str]:
+def parseTSV(tsv_filename: str, header_line: bool=True) -> dict[str: str]:
 	"""
 		Input	: tsv_filename	(str, self explanatory)
 				: header_line		(bool, is there a line with columns' name)
@@ -36,7 +36,7 @@ def ParseTSV(tsv_filename: str, header_line: bool=True) -> dict[int: str]:
 		Output	: dict[int: str], The int is the year, the str is the planning's url
 	"""
 
-	raw_text: str = OpenRawTSV(tsv_filename)
-	output: dict[int: str] = dict(ParseRawText(raw_text, header_line))
+	raw_text: str = openRawTSV(tsv_filename)
+	output: dict[str: str] = dict(parseRawText(raw_text, header_line))
 
 	return output
